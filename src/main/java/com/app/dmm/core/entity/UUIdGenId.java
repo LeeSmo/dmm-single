0663fe69414dbe2a24e061836ea3c0e0503b47c2
李慧根 <1575898486@qq.com>
2021-07-16 18:34:28 +0800

    uuid为负值优化

--- src/main/java/com/app/dmm/core/entity/UUIdGenId.java
+++ src/main/java/com/app/dmm/core/entity/UUIdGenId.java
@@ -11,6 +11,7 @@ public class UUIdGenId implements GenId<Long> {
     @Override
     public Long genId(String s, String s1) {
         //return UUID.randomUUID().toString().replace("-","");
-        return UUID.randomUUID().getLeastSignificantBits();
+        //return UUID.randomUUID().getLeastSignificantBits();
+        return UUID.randomUUID().getMostSignificantBits() & Long.MAX_VALUE;
     }
 }
